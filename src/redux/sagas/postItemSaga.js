34f75af52e75ea18b7ee9bb86f69d74f9cf3ed95
Jordan Walker <jwalker117@gmail.com>
2020-04-23 14:04:15 -0500

    Post saga created

---
+++ src/redux/sagas/postItemSaga.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+//Generator function that uses saga to ajax post request
+function* postItemSaga(action) {
+    console.log('In postItemSaga', action);
+    try {
+        const response = axios.post('/api/shelf', action.payload);
+        yield put({type: 'FETCH_ITEMS'}); //use put in saga and this.props.dispatch in reducers
+    } catch(error) {
+        console.log('error with element get request', error);
+    }
+}
+
+function* postSaga() {
+    yield takeLatest('FETCH_ITEMS', postItemSaga);
+  }
+
+export default postSaga;
