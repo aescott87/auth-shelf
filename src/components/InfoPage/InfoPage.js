d57dff4712d9580f906f1ec72d5dd9860669a2d1
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-04-23 14:05:36 -0500

    info page, GET & POST, inputs, list render

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+class InfoPage extends Component {
+  
+ state = {
+    description: '',
+    image_url: '',
+  }
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Shelf Page
-    </p>
-  </div>
-);
+  componentDidMount () {
+    this.props.dispatch({type:'FETCH_ITEMS'})
+  }
+    
+  handleSubmit =() => {
+    console.log( 'in handleSubmit', this. state);
+    this.props.dispatch({type: 'NEW_OBJECT', payload: this.state})
+  }
+  
+  handleChangeDesc = (event) => {
+    console.log('in handleChangeDesc', event.target.value);
+    this.setState({
+      description: event.target.value
+    })
+  }
 
-export default InfoPage;
+  handleChangeUrl = (event) => {
+    console.log('in handleChangeUrl', event.target.value);
+    this.setState({
+      image_url: event.target.value
+    })
+  }
+  
+  render(){
+    return (
+      <>
+      <div>
+        <p>Shelf Page </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={event => this.handleChangeDesc(event)} placeholder="New Object Description"></input>
+            <input type="text" onChange={event => this.handleChangeUrl(event)} placeholder="Image Url"></input>
+          <button type="submit">Add Button</button>
+        </form>
+        {JSON.stringify(this.props.itemList)}
+        <ul>
+          {this.props.itemList ?
+          this.props.itemList.map(item => {
+            return(
+              <>
+              <li key={item.id}>{item.description}</li>
+              <li><img src={item.url} /></li>
+              </>
+            );
+          })
+            :
+          <p>Nothin!</p>
+          }
+        </ul> 
+      </div>
+      </>
+    );
+  }
+}
+
+const putPropsOnReduxStore = (reduxStore) => ({
+  itemList: reduxStore.getListReducer,
+});
+
+InfoPage = withRouter(InfoPage);
+export default connect(putPropsOnReduxStore)(InfoPage);
